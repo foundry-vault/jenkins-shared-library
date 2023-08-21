b0f07ad92d3f3dec0bd7585e2ec8960ec1415dec
Foundry Vault <foundryvault@gmail.com>
2023-08-21 11:59:42 -0400

    docker build with param

--- vars/buildJar.groovy
+++ vars/buildJar.groovy
@@ -1,6 +1,6 @@
 #!/user/bin/env groovy
 
 def call() {
-    echo 'building the java application with maven...'
+    echo "building the java application with maven for branch $BRANCH_NAME"
     sh 'mvn package'
 }
