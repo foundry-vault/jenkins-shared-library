d8b5dd8231e4e249d80946f94f302ea580db643a
Foundry Vault <foundryvault@gmail.com>
2023-08-29 11:46:27 -0400

    moving functionality to Groovy Class

--- vars/buildJar.groovy
+++ vars/buildJar.groovy
@@ -1,6 +1,6 @@
 #!/user/bin/env groovy
 
 def call() {
-    echo "building the java application with maven for branch $BRANCH_NAME"
+    echo "building the application for branch $BRANCH_NAME"
     sh 'mvn package'
 }
